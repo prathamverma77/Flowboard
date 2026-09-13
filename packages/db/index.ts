7c1f95e6aa44d50632bc36761b8ce72aa5dba027
Pratham <prathamverma1980@gmail.com>
2026-09-13 23:19:04 +0530

    exported the db as a dependency

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,4 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prisma = new PrismaClient();
-export * from "@prisma/client";
+
+
+const adapter = new PrismaPg(
+    { connectionString: process.env.DATABASE_URL }
+);
+
+
+export const prisma = new PrismaClient({adapter});
